47aeaf79f330fb7098f0d9c33a44595f29654680
amdad77682 <amdad77682@gmail.com>
2019-07-02 16:08:17 +0600

    album list

--- config/modules.config.php
+++ config/modules.config.php
@@ -11,7 +11,13 @@
  * This should be an array of module namespaces used in the application.
  */
 return [
+    'Zend\InputFilter',
+    'Zend\Filter',
+    'Zend\Hydrator',
+    'Zend\Form',
+    'Zend\Db',
     'Zend\Router',
     'Zend\Validator',
     'Application',
+    'Album', 
 ];
---
+++ data/load_db.php
@@ -0,0 +1,16 @@
+<?php
+ //require_once 'Zend/Db/Adapter/Pdo/Mysql.php';
+
+// $db = new Zend_Db_Adapter_Pdo_Mysql(array(
+//     'host'     => '127.0.0.1',
+//     'username' => '',
+//     'password' => 'xxxxxxxx',
+//     'dbname'   => 'test'
+// ));
+use Zend\Db\Adapter\AdapterInterface;
+$db = new PDO('sqlite:' . realpath(__DIR__) . '/zftutorial.db');
+$fh = fopen(__DIR__ . '/schema.sql', 'r');
+while ($line = fread($fh, 4096)) {
+    $db->exec($line);
+}
+fclose($fh);
